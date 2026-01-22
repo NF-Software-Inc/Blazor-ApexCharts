a61cf2d38af9ac0c2b9fe73e0de3f0d4d893cc1e
thirstyape <49737478+thirstyape@users.noreply.github.com>
2026-01-22 21:54:45 +0000

    Deploying to gh-pages from master @ 660f7818b29c6f82cef9887582626f3baa8cee47 🚀

--- _content/Blazor-ApexCharts/js/blazor-apexcharts.js
+++ _content/Blazor-ApexCharts/js/blazor-apexcharts.js
@@ -1,4 +1,4 @@
-﻿import ApexCharts from './apexcharts.esm.js?ver=6.0.0.0'
+﻿import ApexCharts from './apexcharts.esm.js?ver=6.1.0.0'
 
 // export function for Blazor to point to the window.blazor_apexchart. To be compatible with the most JS Interop calls the window will be return.
 export function get_apexcharts() {
@@ -313,8 +313,14 @@ window.blazor_apexchart = {
         if (options.tooltip != undefined && options.tooltip.customTooltip == true) {
             options.tooltip.custom = function ({ series, seriesIndex, dataPointIndex, w }) {
 
+                if (dataPointIndex == null) {
+                    dataPointIndex = seriesIndex;
+                    seriesIndex = 0
+                }
+
+
                 var selection = {
-                    dataPointIndex: dataPointIndex,
+                    dataPointIndex: dataPointIndex || 0,
                     seriesIndex: seriesIndex
                 };
 
